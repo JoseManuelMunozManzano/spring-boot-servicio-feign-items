0f180afc517b925fb77180f61d8a3fdbf81a47da
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-15 17:22:46 +0200

    Desacoplando listOfServers

--- src/main/java/com/jmunoz/springboot/app/item/clientes/ProductoClienteRest.java
+++ src/main/java/com/jmunoz/springboot/app/item/clientes/ProductoClienteRest.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-// Con la anotación @FeignClient se indica que esta interface es un cliente Feign, e indicamos el nombre del servicio
-// al que nos queremos conectar y la url con el puerto
-@FeignClient(name="servicio-productos", url="localhost:8001")
+// Usando Ribbon se elimina la parte de la url y tenemos que configurarlo en el archivo properties con Ribbon.
+// Con esto se desacopla aún más la ubicación física (ip, nombre máquina o localhost) y el puerto.
+@FeignClient(name="servicio-productos")
 public interface ProductoClienteRest {
 
     // Aquí en FeignClient se usa @GetMapping para consumir el servicio API REST, obtener los datos JSON convertidos
